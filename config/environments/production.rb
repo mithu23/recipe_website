82c02817c028b0913be5cbfb9523a768672560d9
Mithu23 <mithu.selvakumar@utoronto.ca>
2016-02-09 21:18:30 -0500

    Added caccoon

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,7 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+ Paperclip.options[:command_path] = "/usr/local/bin/"
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
